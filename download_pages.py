977e11815d4c85add3faf22d02f18da5c14cbd46
jorickdefraine <defraine.jorick@gmail.com>
2020-12-11 21:35:22 +0100

    add changing locations

--- download_pages.py
+++ download_pages.py
@@ -1,11 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import requests
 from time import sleep
 import os
-from os import path
-import time
 import urllib.request
 import random
 
@@ -17,26 +13,45 @@ headers = [{
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'},
     {
         'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'}]
+
+locations = ["AR", "DZ", "AM", "AU", "AUADL", "AUBNE", "AUMEL", "AUPER", "AUSYD", "AT", "AZ", "BS", "BD", "BY", "BZ",
+             "BE", "BT", "BA", "BR", "BN", "BG", "CA", "CAYMQ", "CAYTO", "CAYVR", "CL", "CN", "CO", "CR", "HR", "KH",
+             "CZ", "DK", "EE", "EC", "EG", "FI", "FR", "FRPAR", "GE", "GR", "DE", "HK", "HU", "IE", "ID", "IL", "IM",
+             "IN", "IS", "IT", "ITMIL", "ITROM", "JP", "KG", "KZ", "LA", "LI", "LT", "LU", "LV", "ME", "MC", "MD", "MT",
+             "MX", "MY", "NL", "NO", "NP", "NZ", "PA", "PE", "PH", "PK", "PL", "PT", "RO", "RU", "SG", "KR", "ES",
+             "ESBCN", "SE", "SK", "ZA", "CH", "TH", "TR", "TW", "UA", "US", "USATL", "USBOS", "USCLT", "USCHI", "USCOL",
+             "USDAL", "USHOU", "USIND", "USMCI", "USLAS", "USLAX", "USMIA", "USEWR", "USNYC", "USORD", "USPHL", "USPHX",
+             "USPDX", "USSFO", "USSJC", "USSEA", "USWAS", "UY", "AE", "GB", "GBCVT", "VE", "VN"]
 print(headers[random.randrange(5)])
+
+
+def change_locations():
+    os.system("hotspotshield disconnect")
+    location = locations[random.randrange(len(locations))]
+    print(location)
+    sleep(5)
+    os.system("hotspotshield connect " + location)
+
 
 if __name__ == "__main__":
     links = []
-    for n in range(20, 3902):
+    for n in range(1, 3902):
+        if not (n % 10):
+            print("start changing location")
+            change_locations()
+            print("end changing location")
         try:
             sleep(random.randint(1, 3))
-            r = requests.get('https://www.lacentrale.fr/listing?makesModelsCommercialNames=&options=&page=' + str(
-                n) + '&regions=FR-IDF', headers=headers[random.randrange(5)])
+            r = requests.get('https://www.lacentrale.fr/listing?makesModelsCommercialNames=&options=&page=' + str(n) + '&regions=FR-IDF', headers=headers[random.randrange(5)])
             soup = BeautifulSoup(r.text, features='html.parser')
-            #print(soup)
             anchors = soup.find_all('a', class_='searchCard__link')
-            print(anchors)
             cnt = 0
             print("success on first link")
             print(n)
             for anchor in anchors:
+                print(anchor['href'])
                 cnt += 1
                 try:
-                    links.append("https://www.lacentrale.fr" + anchor['href'])
                     response = urllib.request.urlopen("https://www.lacentrale.fr" + anchor['href'])
                     webContent = response.read()
                     f = open('./data' + anchor['href'], 'wb')
