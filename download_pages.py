aef8763569a26dc6b8f7eb5b864e8f36b90ade40
jorickdefraine <defraine.jorick@gmail.com>
2020-12-12 13:25:53 +0100

    update download with Mootez's script

--- download_pages.py
+++ download_pages.py
@@ -4,6 +4,8 @@ from time import sleep
 import os
 import urllib.request
 import random
+from lxml import html
+import pandas as pd
 
 headers = [{
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'},
@@ -32,16 +34,90 @@ def change_locations():
     os.system("hotspotshield connect " + location)
 
 
+# Scraping using requests+Xpath with changing header for each url
+
+def scrap_subcar_requests(url):
+    req = requests.get(url, headers=headers[random.randrange(5)])
+    doc = html.fromstring(req.text)
+
+    try:
+        name = doc.xpath('/html/body/section/section[1]/div[3]/div/div/div[1]')[0].text_content().replace('\n', '')
+    except:
+        name = 'NAN'
+    try:
+        model = doc.xpath('//*[@id="generalInformation"]/h1')[0].text_content().replace(name, '')
+    except:
+        model = 'NAN'
+    try:
+        Prix = doc.xpath('/html/body/section/section[1]/div[3]/div/div/div[2]/div/span/span')[0].text_content().replace(
+            '\xa0', '').replace('\n', '')
+    except:
+        Prix = 'NAN'
+    try:
+        Mise_en_circluation = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[1]/li[2]/span[2]')[0].text_content()
+    except:
+        Mise_en_circluation = 'NAN'
+    try:
+        Kilométrage_compteur = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[1]/li[4]/span[2]')[0].text_content()
+    except:
+        Kilométrage_compteur = 'NAN'
+    try:
+        Energie = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[1]/li[5]/span[2]')[0].text_content()
+    except:
+        Energie = 'NAN'
+    try:
+        Boite_de_vitesse = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[1]/li[6]/span[2]')[0].text_content()
+    except:
+        Boite_de_vitesse = 'NAN'
+    try:
+        Nombre_de_porte = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[1]/li[8]/span[2]')[0].text_content()
+    except:
+        Nombre_de_porte = 'NAN'
+    try:
+        Nombre_de_place = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[1]/li[9]/span[2]')[0].text_content()
+    except:
+        Nombre_de_place = 'NAN'
+    try:
+        Puissance_fiscale = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[2]/li[5]/span[2]')[
+            0].text_content()  # .replace('CV','')
+    except:
+        Puissance_fiscale = 'NAN'
+    try:
+        Puissance_din = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[2]/li[6]/span[2]')[0].text_content()
+    except:
+        Puissance_din = 'NAN'
+    try:
+        Consommation_mixte = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[2]/li[9]/span[2]')[0].text_content()
+    except:
+        Consommation_mixte = 'NAN'
+
+    try:
+        Emission_CO2 = doc.xpath('//*[@id="generalInformation"]/div[2]/ul[2]/li[8]/span[2]')[
+            0].text_content()  # .replace('g/kmB','') if we want to delete the suffix
+    except:
+        Emission_CO2 = 'NAN'
+
+    result = {'nom': name, 'model': model, 'price': Prix, 'Mise_en_circluation': Mise_en_circluation,
+              'Kilométrage_compteur': Kilométrage_compteur, 'Energie': Energie, 'Boite_de_vitesse': Boite_de_vitesse,
+              'Nombre_de_porte': Nombre_de_porte, 'Nombre_de_place': Nombre_de_place,
+              'Puissance_fiscale': Puissance_fiscale, 'Puissance_din': Puissance_din,
+              'Consommation_mixte': Consommation_mixte, 'Emission_CO2': Emission_CO2}
+
+    return result
+
+
 if __name__ == "__main__":
-    links = []
-    for n in range(1, 5):
-        if not (n % 2):
+    results = []
+    for n in range(1, 15):
+        if not (n % 5):
             print("start changing location")
             change_locations()
+            sleep(3)
             print("end changing location")
         try:
             sleep(random.uniform(1, 3))
-            r = requests.get('https://www.lacentrale.fr/listing?makesModelsCommercialNames=&options=&page=' + str(n) + '&regions=FR-IDF', headers=headers[random.randrange(5)])
+            r = requests.get('https://www.lacentrale.fr/listing?makesModelsCommercialNames=&options=&page=' + str(
+                n) + '&regions=FR-IDF', headers=headers[random.randrange(5)])
             soup = BeautifulSoup(r.text, features='html.parser')
             anchors = soup.find_all('a', class_='searchCard__link')
             cnt = 0
@@ -52,11 +128,13 @@ if __name__ == "__main__":
                     print(anchor['href'])
                     cnt += 1
                     sleep(random.uniform(1, 3))
-                    response = urllib.request.urlopen("https://www.lacentrale.fr" + anchor['href'])
-                    webContent = response.read()
-                    f = open('./data' + anchor['href'], 'wb')
-                    f.write(webContent)
-                    f.close
+                    r = scrap_subcar_requests("https://www.lacentrale.fr" + anchor['href'])
+                    results.append(r)
+                    #response = urllib.request.urlopen("https://www.lacentrale.fr" + anchor['href'])
+                    #webContent = response.read()
+                    #f = open('./data' + anchor['href'], 'wb')
+                    #f.write(webContent)
+                    #f.close
                     print("success on second link")
                     print(cnt)
 
@@ -69,3 +147,6 @@ if __name__ == "__main__":
         except:
             print("failed on first link")
             print(n)
+
+    df = pd.DataFrame(results)
+    df.to_csv('Cars_DB.csv', mode='a')
